7fc392cec7c8d28979c8f8658b1bc5d70b43f61b
test <brucemclarenp4@gmail.com>
2025-10-23 00:26:17 +0900

    index作成

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen text-white text-center">
+    <h1 class="text-4xl font-bold mb-4">HINASELECT for 日向坂46</h1>
+    <p class="text-lg opacity-80 mb-6">あなたの推しメンを診断します！</p>
+
+    <NuxtLink
+      to="/question"
+      class="bg-white/20 hover:bg-white/30 text-white px-6 py-3 rounded-xl transition"
+    >
+      診断をはじめる
+    </NuxtLink>
+  </div>
+</template>
