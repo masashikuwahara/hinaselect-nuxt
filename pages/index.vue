a798c4c5e912fe7727fd904ca0bc3865742e51b5
test <brucemclarenp4@gmail.com>
2025-10-24 00:26:58 +0900

    トップページ修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen text-white text-center">
     <h1 class="text-4xl font-bold mb-4">HINASELECT for 日向坂46</h1>
-    <p class="text-lg opacity-80 mb-6">あなたの推しメンを診断します！</p>
-
-    <NuxtLink
+    <!-- <p class="text-lg opacity-80 mb-6">あなたの推しメンを診断します！</p> -->
+    <p class="text-lg opacity-80 mb-6">メンテナンス中です。近日リリース予定。</p>
+    <img src="/images/000.png" alt="なおみく" class="w-64 md:w-64 lg:w-80 h-auto mx-auto" />
+    <!-- <NuxtLink
       to="/question"
       class="bg-white/20 hover:bg-white/30 text-white px-6 py-3 rounded-xl transition"
     >
       診断をはじめる
-    </NuxtLink>
+    </NuxtLink> -->
   </div>
 </template>
