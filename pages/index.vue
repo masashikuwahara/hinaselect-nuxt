ee23efbaab08d5b5282b2a112cf51cb8b2f3b477
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2025-10-25 23:45:48 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-bold mb-4">HINASELECT for 日向坂46</h1>
     <!-- <p class="text-lg opacity-80 mb-6">あなたの推しメンを診断します！</p> -->
     <p class="text-lg opacity-80 mb-6">メンテナンス中です。近日リリース予定。</p>
-    <img src="/images/000.png" alt="なおみく" class="w-64 md:w-64 lg:w-80 h-auto mx-auto" />
+    <img src="/images/000.jpg" alt="なおみく" class="w-64 md:w-64 lg:w-80 h-auto mx-auto" />
     <!-- <NuxtLink
       to="/question"
       class="bg-white/20 hover:bg-white/30 text-white px-6 py-3 rounded-xl transition"
@@ -12,3 +12,4 @@
     </NuxtLink> -->
   </div>
 </template>
+
